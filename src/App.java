ec45434c17eb227848cc56559c2c03c075d9b958
BlueMadnessVG <102503098+BlueMadnessVG@users.noreply.github.com>
2023-02-01 18:44:52 -0700

    Matrix librarys

--- src/App.java
+++ src/App.java
@@ -1,36 +1,34 @@
+import library.Matrix;
+import library.MatrixMath;
+
+
+
 public class App {
-    //datos de entrada
-    public static int inputs[][] = { { 1 , 1 }, { 1 , 0 }, { 0 , 1 }, { 0 , 0 } };
+    
+    //INPUT MATRIX INITIALIZATION
+    public static double[][] i = { {1,1}, {1,0}, {0,1}, {0,0}} ;
+    public static Matrix inputs = new Matrix(i);
+    //WEIGHT MATRIX INITIALIZATION
+    public static double[][] weight = { {1, 1, -1, 1}, {1, 1, 1, -1} }; 
+    public static Matrix w = new Matrix(weight);
+
     //variables de los humbrales
     public static double threshold[] = { 0.5, 1.5, -0.5 };
 
-    // calcula el peso del la entrada
-    public static double weightCal( int w[], int iv1, int iv2 ){
-        
-        //CALCULO DE I * W
-        double i1 = iv1 * w[ 0 ];
-        double i2 = iv2 * w[ 1 ];
-        //suma de los resultados
-        return (i1 + i2);
-    }
-
     //NEURONA PARA OR
-    public static int OR( int iv1, int iv2 ) {
+    public static int OR( double result ) {
         //Pesos de la neurona
-        int w[] = { 1, 1 };
         //verifica si el index pasa el humbral
-        if( weightCal(w, iv1, iv2) > threshold[0] )
+        if( result > threshold[0] )
             return 1;
         else
             return 0;
     }
     //NEURONA PARA AND
-    public static int AND( int iv1, int iv2 ) {
-
+    public static int AND( double result ) {
         //Pesos de la neurona
-        int w[] = { 1, 1 };
         //verifica si el index pasa el humbral
-        if( weightCal(w, iv1, iv2) > threshold[1] )
+        if( result > threshold[1] )
             return 1;
         else
             return 0;
@@ -39,9 +37,11 @@ public class App {
     //NEURONA PARA XOR
     public static int XOR( int iv1, int iv2 ) {
         //Pesos de la neurona
-        int w[] = { -1, 1 };
+        double resultI[] = {iv1, iv2};
+        Matrix aux = Matrix.createRowMatrix( resultI );
+
         //verifica si el index pasa el humbral
-        if( weightCal(w, AND(iv1, iv2), OR(iv1, iv2)) > threshold[0] ) 
+        if( MatrixMath.dotProduct(aux, w.getCol(2)) > threshold[0] ) 
             return 1;
         else 
             return 0;
@@ -50,22 +50,23 @@ public class App {
 
     public static int EQ( int iv1, int iv2 ) {
         
-        int w[] = { 1 , -1 };
-        if( weightCal(w, AND(iv1, iv2), OR(iv1, iv2)) > threshold[2] ) 
+        double resultI[] = {iv1, iv2};
+        Matrix aux = Matrix.createRowMatrix( resultI );
+        if( MatrixMath.dotProduct(aux, w.getCol(3)) > threshold[2] ) 
             return 1;
         else
             return 0;
     }
 
     public static void main(String[] args) throws Exception {
-        
-        System.out.println();
-        System.out.println( "|       A      |       B       |       AND     |       OR      |       XOR      |       EQ       |" );
+
+        System.out.println( );
+        System.out.println( "|       A        |       B         |       AND     |       OR      |       XOR      |       EQ       |" );
 
         //Ciclo para recorrer los valores de entrada ( inputs )
-        for( int i = 0; i < 4; i++ ) {
+        for( int i = 0; i < inputs.getRows(); i++ ) {
 
-            System.out.println( "|       " + inputs[i][0] + "      |       " + inputs[i][1] + "       |        "  + AND(inputs[i][0], inputs[i][1]) + "      |       " + OR(inputs[i][0], inputs[i][1]) +  "       |        " + XOR(inputs[i][0], inputs[i][1]) +  "       |        " + EQ( inputs[i][0], inputs[i][1]) + "       |" );
+            System.out.println( "|       " + inputs.get(i, 0) + "      |       " + inputs.get(i, 1) + "       |        "  + AND( MatrixMath.dotProduct(inputs.getRow(i), w.getCol(0)) ) + "      |       " + OR( MatrixMath.dotProduct(inputs.getRow(i), w.getCol(1)) ) +  "       |        " + XOR(AND( MatrixMath.dotProduct(inputs.getRow(i), w.getCol(0)) ), OR( MatrixMath.dotProduct(inputs.getRow(i), w.getCol(1)) ) ) +  "       |        " + EQ(AND( MatrixMath.dotProduct(inputs.getRow(i), w.getCol(0)) ), OR( MatrixMath.dotProduct(inputs.getRow(i), w.getCol(1)) ) ) + "       |" );
 
         }
 
