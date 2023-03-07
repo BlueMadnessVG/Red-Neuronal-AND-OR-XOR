d228431a02da3b4da03bb7a82bbe2650d3af0fa2
BlueMadnessVG <102503098+BlueMadnessVG@users.noreply.github.com>
2023-03-07 01:00:20 -0700

    backpropagation

--- src/App.java
+++ src/App.java
@@ -6,12 +6,19 @@ import library.MatrixMath;
 public class App {
     
     //INPUT MATRIX INITIALIZATION
-    public static double[][] i = { {0,0,1}, {0,1,1}, {1,0,1}, {1,1,1}} ;
-    public static int[][] r = { {0, 0, 0, 1}, {0, 1, 1, 1}, {0, 1, 1, 0}, {1, 0, 0, 1} };
+    public static double[][] i = { {1,0,1}, {0,1,1}, {0,0,1}, {1,1,1}} ;
+    public static int[][] r = { {0, 0, 0, 1}, {0, 0, 1, 1}, {0, 0, 1, 1}, {1, 0, 0, 1} };
     public static Matrix inputs = new Matrix(i);
     //WEIGHT MATRIX INITIALIZATION
-    public static double[][] weight = { {-2.468, 4.61, -4.3665, 1}, {-2.438, 4.636, 8.17533, -1}, {1.1162, -2.254, -1.8211, 1} }; 
+    public static double[][] weight = { {-0.07, 0.94, -0.22, 1}, {0.22, 0.46, 0.58, -1}, {-0.46, 0.1, 0.78, 1} }; 
     public static Matrix w = new Matrix(weight);
+
+    public static double[] deltas = { 0, 0, 0 };
+    public static double[] DeltaGrad = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    public static double[] DeltaHist = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    public static double LR = 0.7;
+    public static double Momentum = 0.3;
 
     //variables de los humbrales
     public static double threshold[] = { 0.5, 1.5, -0.5 };
@@ -19,6 +26,44 @@ public class App {
     //Funcion de activacion
     public static double AF( double result ) {
         return ( 1 / ( 1 + Math.exp( -result ) ) );
+    }
+
+    //Claculo de delta exterior
+    public static void EDelta( double af, double err ) {
+        deltas[2] = -err * ( af * ( 1 - af ) );
+    }
+
+    //Calculo de delta interior
+    public static void IDelta( double af1, double af2, double w1, double w2 ) {
+        deltas[0] = ( af1 * ( 1 - af1 ) ) * deltas[2] * w1;
+        deltas[1] = ( af2 * ( 1 - af2 ) ) * deltas[2] * w2;
+    }
+
+    //Claculo Historico de delta
+    public static void HDelta(){
+
+        for( int i = 0; i < 9; i++ ){
+            DeltaHist[i] = ( LR * DeltaGrad[i] ) + ( Momentum * DeltaHist[i] );
+
+        }
+
+    } 
+
+    //ReValanseo de pesos
+    public static void NewWeights() {
+        int index = 0;
+        for( int i = 0; i < 3; i++ ){
+
+            for( int x = 0; x < 3; x++ ){
+
+                System.out.println( weight[x][i] + "            " + (index) );
+                weight[x][i] += DeltaHist[index];
+                System.out.println(weight[x][i]);
+                index++;
+            }
+
+        }
+
     }
 
     //NEURONA PARA OR
@@ -58,7 +103,7 @@ public class App {
         Double sum = 0.0;
 
         for( int x = 0; x < 4; x++ ){
-            if ( x == 0) {
+            /* if ( x == 0) {
                 System.out.println( "|       A        |       B         |                AND              |      IDEAL     |        ERROR       " );
                 //Ciclo para recorrer los valores de entrada ( inputs )
                 for( int i = 0; i < inputs.getRows(); i++ ) {
@@ -88,14 +133,48 @@ public class App {
                 System.out.println("|           SUMATORIA         |               ESS           |               MSE          |               RMS            |");
                 System.out.println("|       " + sum + "     |       " + (sum / 2) + "     |       " + ((sum / 4) * 100) + "%    |       " + (Math.sqrt(sum / 4) * 100) + "%     |");
                 System.out.println( );   
-            }
-            else if ( x == 2) {
-                System.out.println( "|       A        |       B         |       XOR" );
+            } */
+            if ( x == 2) {
+                System.out.println( "|       A        |       B         |       XOR        |               O1 DELTA            |" );
                 //Ciclo para recorrer los valores de entrada ( inputs )
                 for( int i = 0; i < inputs.getRows(); i++ ) {
-                    Double aux = XOR(AND( MatrixMath.dotProduct(inputs.getRow(i), w.getCol(0)) ), OR( MatrixMath.dotProduct(inputs.getRow(i), w.getCol(1)) ) );
-                    Double error = r[1][i] - aux;
-                    System.out.println( "|       " + inputs.get(i, 0) + "      |       " + inputs.get(i, 1) + "       |        " + aux + "       |        " + r[1][i] + "       |        " + Math.round(Math.abs( (error) * 100 ) ) + " %" );
+
+                    Double af1 = AF( MatrixMath.dotProduct(inputs.getRow(i), w.getCol(0)) );
+                    Double af2 = AF( MatrixMath.dotProduct(inputs.getRow(i), w.getCol(1)) );
+
+                    Double aux = XOR( af1 , af2 );
+                    Double error = aux - r[1][i];
+
+                    EDelta(aux, error);
+                    IDelta(af1, af2, weight[0][2], weight[1][2]);
+
+                    for( int z = 0; z < 9; z++ ){
+
+                        if( z >= 6 ){
+                            DeltaGrad[z] += ( deltas[2] * af1 );
+                            z++;
+                            DeltaGrad[z] += ( deltas[2] * af2 );
+                            z++;
+                            DeltaGrad[z] += ( deltas[2] * 1 );
+                        }
+                        else if ( z >= 3 ){
+                            DeltaGrad[z] += ( deltas[1] * inputs.get(i, 0) );
+                            z++;
+                            DeltaGrad[z] += ( deltas[1] * inputs.get(i, 1) );
+                            z++;
+                            DeltaGrad[z] += ( deltas[1] * 1 );
+                        }
+                        else {
+                            DeltaGrad[z] += ( deltas[0] * inputs.get(i, 0) );
+                            z++;
+                            DeltaGrad[z] += ( deltas[0] * inputs.get(i, 1) );
+                            z++;
+                            DeltaGrad[z] += ( deltas[0] * 1 ); 
+                        }
+                 
+                    }
+
+                    System.out.println( "|       " + inputs.get(i, 0) + "      |       " + inputs.get(i, 1) + "       |        " + aux + "       |        " + r[1][i] + "       |        " + error + "       |        " + deltas[2]);
                     sum += Math.pow( error, 2);
                 }
 
@@ -103,8 +182,12 @@ public class App {
                 System.out.println("|           SUMATORIA         |               ESS           |               MSE          |               RMS            |");
                 System.out.println("|       " + sum + "     |       " + (sum / 2) + "     |       " + ((sum / 4) * 100) + "%    |       " + (Math.sqrt(sum / 4) * 100) + "%     |");
                 System.out.println( );   
+
+                HDelta();
+                NewWeights();
+
             }
-            else {
+/*             else {
                 System.out.println( "|       A        |       B         |       EQ" );
                 //Ciclo para recorrer los valores de entrada ( inputs )
                 for( int i = 0; i < inputs.getRows(); i++ ) {
@@ -117,8 +200,8 @@ public class App {
                 System.out.println( );
                 System.out.println("|           SUMATORIA         |               ESS           |               MSE          |               RMS            |");
                 System.out.println("|       " + sum + "     |       " + (sum / 2) + "     |       " + ((sum / 4) * 100) + "%    |       " + (Math.sqrt(sum / 4) * 100) + "%     |");
-                System.out.println( );   
-            }
+                */ System.out.println( );   
+            //}
             sum = 0.0;
         }
 
